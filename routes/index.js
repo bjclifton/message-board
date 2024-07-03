51e5f64870a2db5b8f109a86643f3ff7abdc9372
Bradley Clifton <bclifton@nhaschools.com>
2024-07-03 09:18:55 -0400

    Simple message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',{messages});
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function (req, res, next) {
+  const author = req.body.author;
+  const message = req.body.message;
+  messages.push({ text: message, user: author, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
